d9b1d4f2174c1cf1955cb1763216124b53f8a73b
Prithwijit Ghosh <prith.tiu13@gmail.com>
2026-03-24 15:33:41 +0530

    Cleaned the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes, Navigate, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/contexts/ThemeContext";
@@ -23,11 +29,8 @@ import BankMaster from "./pages/masters/BankMaster";
 import ExpensesMaster from "./pages/masters/ExpensesMaster";
 import ItemMaster from "./pages/masters/ItemMaster";
 import ItemGroupMaster from "./pages/masters/ItemGroupMaster";
-import AccountGroupMaster from "./pages/setup/AccountGroupMaster";
-import AccountHeadMaster from "./pages/setup/AccountHeadMaster";
 
 import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminModule from "./pages/admin/AdminModule";
 import MenuRights from "./pages/admin/MenuRights";
 import WidgetsRights from "./pages/admin/WidgetsRights";
 import FinYearRights from "./pages/admin/FinYearRights";
@@ -64,30 +67,182 @@ const PublicRoute = ({ children }: { children: React.ReactNode }) => {
 
 const AppRoutes = () => (
   <Routes>
-    <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
-    <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-    <Route path="/transactions" element={<ProtectedRoute><Transactions /></ProtectedRoute>} />
-    <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
-    <Route path="/reports" element={<ProtectedRoute><Reports /></ProtectedRoute>} />
-    <Route path="/widgets" element={<ProtectedRoute><Widgets /></ProtectedRoute>} />
-    <Route path="/tasks" element={<ProtectedRoute><Tasks /></ProtectedRoute>} />
-    <Route path="/tasks/:id" element={<ProtectedRoute><TaskDetail /></ProtectedRoute>} />
-    <Route path="/masters/contractors" element={<ProtectedRoute><ContractorMaster /></ProtectedRoute>} />
-    <Route path="/masters/suppliers" element={<ProtectedRoute><SupplierMaster /></ProtectedRoute>} />
-    <Route path="/masters/customers" element={<ProtectedRoute><CustomerMaster /></ProtectedRoute>} />
-    <Route path="/masters/banks" element={<ProtectedRoute><BankMaster /></ProtectedRoute>} />
-    <Route path="/masters/expenses" element={<ProtectedRoute><ExpensesMaster /></ProtectedRoute>} />
-    <Route path="/masters/items" element={<ProtectedRoute><ItemMaster /></ProtectedRoute>} />
-    <Route path="/masters/item-groups" element={<ProtectedRoute><ItemGroupMaster /></ProtectedRoute>} />
-    <Route path="/setup/account-groups" element={<ProtectedRoute><AccountGroupMaster /></ProtectedRoute>} />
-    <Route path="/setup/account-heads" element={<ProtectedRoute><AccountHeadMaster /></ProtectedRoute>} />
-    <Route path="/admin" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-    <Route path="/admin/dashboard" element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>} />
-    <Route path="/admin/rights/menu" element={<ProtectedRoute><MenuRights /></ProtectedRoute>} />
-    <Route path="/admin/rights/widgets" element={<ProtectedRoute><WidgetsRights /></ProtectedRoute>} />
-    <Route path="/admin/rights/fin-year" element={<ProtectedRoute><FinYearRights /></ProtectedRoute>} />
-    <Route path="/admin/approval/setup" element={<ProtectedRoute><ApprovalSetup /></ProtectedRoute>} />
-    <Route path="/admin/approval/post-rights" element={<ProtectedRoute><PostApprovalRights /></ProtectedRoute>} />
+    <Route
+      path="/login"
+      element={
+        <PublicRoute>
+          <LoginPage />
+        </PublicRoute>
+      }
+    />
+    <Route
+      path="/"
+      element={
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/transactions"
+      element={
+        <ProtectedRoute>
+          <Transactions />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/users"
+      element={
+        <ProtectedRoute>
+          <Users />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/reports"
+      element={
+        <ProtectedRoute>
+          <Reports />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/widgets"
+      element={
+        <ProtectedRoute>
+          <Widgets />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/tasks"
+      element={
+        <ProtectedRoute>
+          <Tasks />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/tasks/:id"
+      element={
+        <ProtectedRoute>
+          <TaskDetail />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/masters/contractors"
+      element={
+        <ProtectedRoute>
+          <ContractorMaster />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/masters/suppliers"
+      element={
+        <ProtectedRoute>
+          <SupplierMaster />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/masters/customers"
+      element={
+        <ProtectedRoute>
+          <CustomerMaster />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/masters/banks"
+      element={
+        <ProtectedRoute>
+          <BankMaster />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/masters/expenses"
+      element={
+        <ProtectedRoute>
+          <ExpensesMaster />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/masters/items"
+      element={
+        <ProtectedRoute>
+          <ItemMaster />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/masters/item-groups"
+      element={
+        <ProtectedRoute>
+          <ItemGroupMaster />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin"
+      element={
+        <ProtectedRoute>
+          <AdminDashboard />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/dashboard"
+      element={
+        <ProtectedRoute>
+          <AdminDashboard />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/rights/menu"
+      element={
+        <ProtectedRoute>
+          <MenuRights />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/rights/widgets"
+      element={
+        <ProtectedRoute>
+          <WidgetsRights />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/rights/fin-year"
+      element={
+        <ProtectedRoute>
+          <FinYearRights />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/approval/setup"
+      element={
+        <ProtectedRoute>
+          <ApprovalSetup />
+        </ProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/approval/post-rights"
+      element={
+        <ProtectedRoute>
+          <PostApprovalRights />
+        </ProtectedRoute>
+      }
+    />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
