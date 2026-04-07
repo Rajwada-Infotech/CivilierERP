515e14d8f04a4896af27f9128800d66e3567c8f6
Prithwijit Ghosh <prith.tiu13@gmail.com>
2026-04-07 11:29:28 +0530

    backend server

--- backend/routes/dba.js
+++ backend/routes/dba.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getPool } = require("../db");
+const { getPool, sql } = require("../db");
 
 // ======================
 //  DBA ROUTES - SECURED
