2f63e3644110c49265f727d1287a19f7d30a14e8
Prithwijit Ghosh <prith.tiu13@gmail.com>
2026-05-19 16:02:47 +0530

    Sales Deed

--- backend/server.js
+++ backend/server.js
@@ -130,6 +130,7 @@ const ALL_ROUTES = [
   { path: "/api/followup-agreements", file: "./routes/followupAgreements" },
   { path: "/api/followup-noc", file: "./routes/followupNoc" },
   { path: "/api/followup-sales-deed", file: "./routes/followupSalesDeed" },
+  { path: "/api/followup-handover", file: "./routes/followupHandover" },
   { path: "/api/company-master", file: "./routes/companyMaster" },
   { path: "/api/project-master", file: "./routes/projectMaster" },
   { path: "/api/signatures", file: "./routes/signatures" },
