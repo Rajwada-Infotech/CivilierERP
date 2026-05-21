d74e8698b2da636e5c35b4f56a57dcfc00779e93
Prithwijit Ghosh <prith.tiu13@gmail.com>
2026-05-21 17:04:41 +0530

    Fixed Dead Code

--- src/App.tsx
+++ src/App.tsx
@@ -154,9 +154,6 @@ const UnitOfMeasurementMaster = lazy(
   () => import("./pages/material/UnitOfMeasurementMaster"),
 );
 const InventoryMaster = lazy(() => import("./pages/material/InventoryMaster"));
-const AccessCardMaster = lazy(
-  () => import("./pages/material/AccessCardMaster"),
-);
 const Stock = lazy(() => import("./pages/material/Stock"));
 const StockTransfer = lazy(() => import("./pages/material/StockTransfer"));
 const EnterpriseMasterPage = lazy(
@@ -947,14 +944,6 @@ function AppRoutes() {
         element={
           <ProtectedRoute>
             <InventoryMaster />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/material/access-card-master"
-        element={
-          <ProtectedRoute>
-            <AccessCardMaster />
           </ProtectedRoute>
         }
       />
