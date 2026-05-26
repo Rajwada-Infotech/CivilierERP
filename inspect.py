5cb9db782b7d18fd22d12594fcc10d4451dacee9
Rahul <rahuladhikary077@gmail.com>
2026-05-26 13:59:21 +0530

    chore: remove temporary Python fix scripts

--- inspect.py
+++
@@ -1,5 +0,0 @@
-with open('src/hooks/useReminders.ts', 'rb') as f:
-    c = f.read()
-
-idx = c.find(b'isFetching.current = true')
-print(repr(c[idx-10:idx+120]))
--- inspect2.py
+++
@@ -1,4 +0,0 @@
-with open('src/hooks/useReminders.ts', 'rb') as f:
-    c = f.read()
-idx = c.find(b'useEffect(() => {')
-print(repr(c[idx:idx+100]))
