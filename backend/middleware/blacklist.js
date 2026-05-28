8fdc67d45a0e7af7f4c22e2494b9144071deba9f
Rahul <rahuladhikary077@gmail.com>
2026-05-28 12:29:28 +0530

    ENGINEER LOGINS AND PERMISSIONS ALSO WORKED ON MENU RIGHTS AND USER RIGHTS CONTROLS

--- backend/middleware/blacklist.js
+++ backend/middleware/blacklist.js
@@ -36,8 +36,6 @@ async function checkBlacklist(req, res, next) {
   let token = null;
   if (req.headers.authorization?.startsWith("Bearer ")) {
     token = req.headers.authorization.split(" ")[1];
-  } else if (req.query.token) {
-    token = req.query.token;
   }
 
   if (!token) return next();
