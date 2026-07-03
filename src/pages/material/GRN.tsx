faed8ddd5f43d64da62ab8862b87e0e58dd618f1
grenxnja <prith.tiu13@gmail.com>
2026-07-03 13:28:23 +0530

    fix: expand GRN filter grid to 5 cols to show Company dropdown

--- src/pages/material/GRN.tsx
+++ src/pages/material/GRN.tsx
@@ -1564,7 +1564,7 @@ export default function GRN() {
                   sub="Narrow the list, then select a PO"
                 />
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
                   {/* Company */}
                   <div>
                     <FieldLabel>Company</FieldLabel>
